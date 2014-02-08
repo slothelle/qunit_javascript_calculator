1053f32a28af5396cffc1f4e45f39a0d6476c6ed
Liz Abinante <eabinante@gmail.com>
2014-02-08 13:56:39 -0600

    tests for push

--- qunit-tests.js
+++ qunit-tests.js
@@ -4,3 +4,14 @@ test( 'init prototype', function() {
   ok( calc, Calculator, 'Is a Calculator' );
   ok( calc.nums, [], 'Contains no numbers on init');
 });
+
+test('.push()', function() {
+  calc.push(1);
+  ok( calc.nums.length, 1, 'Contains one item');
+  ok( calc.nums, [1], 'Contains specific item added');
+
+  calc.push(2);
+  calc.push(4);
+  ok( calc.nums.length, 3, 'Accumulates items');
+  ok( calc.nums, [1, 2, 4], 'Stores items in order inserted');
+});
